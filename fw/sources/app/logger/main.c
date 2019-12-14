135532c96a0db363e71c8d15fe17fe50220872e8
Jakub Kaderka <jakub.kaderka@gmail.com>
2019-12-14 10:08:46 +0100

    Updated main file to do something

--- fw/sources/app/logger/main.c
+++ fw/sources/app/logger/main.c
@@ -1,3 +1,30 @@
+/*
+ * Copyright (C) 2019 Jakub Kaderka
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file    app/logger/main.c
+ * @brief   Main file for GLogger app
+ *
+ * @addtogroup utils
+ * @{
+ */
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/i2c.h>
+#include <libopencm3/stm32/spi.h>
 
 #include <hal/io.h>
 #include <hal/i2c.h>
@@ -6,6 +33,7 @@
 #include <hal/wdg.h>
 #include <hal/usb_msc.h>
 
+#include <modules/log.h>
 #include <drivers/spi_flash.h>
 #include <drivers/ssd1306.h>
 #include <drivers/gps.h>
@@ -13,6 +41,37 @@
 #include <drivers/ramdisk.h>
 #include "storage.h"
 #include "stats.h"
+#include "gui.h"
+#include "version.h"
+
+/**
+ * Configure system clock
+ *
+ * External 16 MHz used to generate precise 48 MHz for USB
+ * System clock running directly from crystal (16 MHz)
+ */
+static void setClock(void)
+{
+    /* Enable crystal oscillator and use it as a system clock */
+    rcc_osc_on(RCC_HSE);
+    rcc_wait_for_osc_ready(RCC_HSE);
+    rcc_set_sysclk_source(RCC_HSE);
+
+    rcc_apb1_frequency = 16000000;
+    rcc_ahb_frequency = 16000000;
+
+    /* No prescalers for system clock and peripherals */
+    rcc_set_hpre(RCC_CFGR_HPRE_NODIV);
+    rcc_set_ppre(RCC_CFGR_PPRE_NODIV);
+
+    /* Setup PLL to generate 48 MHz for usb */
+    rcc_set_pll_multiplication_factor(RCC_CFGR_PLLMUL_MUL6);
+    rcc_set_pll_source(RCC_CFGR_PLLSRC_HSE_CLK);
+    rcc_set_prediv(RCC_CFGR2_PREDIV_DIV2);
+    rcc_osc_on(RCC_PLL);
+    rcc_wait_for_osc_ready(RCC_PLL);
+    rcc_set_usbclk_source(RCC_PLL);
+}
 
 static void addReadme(void)
 {
@@ -26,6 +85,15 @@ static void loop(void)
 {
     gps_info_t *gps;
 
+    // turn on the gps
+    // while gps not valid
+        // sleep until woken up by interrupt, process nema, if not complete, sleep
+    //store gps position
+    //turn off the gps
+    //go to stop mode, wait for rtc to wake mcu up
+
+    //wake up sources - buttons in on mode, holding button in off mode
+
     gps = Gps_Get();
     if (gps) {
         Stats_Update(gps);
@@ -37,28 +105,44 @@ static void loop(void)
 
 int main(void)
 {
-    //TODO initialize clock - 48MHz for usb with clock synchronization */
-
+    /* Initialize clock system, IO pins and systick */
+    setClock();
     IOd_Init();
-    I2Cd_Init(1, true);
-    SPId_Init(1, SPID_PRESC_8, SPI_MODE_0);
-    UARTd_Init(USART_GPS_TX, 9600);
     Time_Init();
 
-    Gps_Init();
-    SpiFlash_WriteEnable();
-    SSD1306_Init();
-    Storage_Init();
-    Stats_Init();
+    /* early init - debug output */
+    UARTd_Init(USART_DEBUG_TX, 115200);
+    Log_SetLevel(LOG_DEBUG);
+    Log_Info(NULL, "GLogger, fw version %d.%d", FW_MAJOR, FW_MINOR);
+    Log_Info(NULL, "Deadbadger.cz");
+
+    I2Cd_Init(1, true);
+    SPId_Init(1, SPID_PRESC_2, SPI_MODE_0);
+    UARTd_Init(USART_GPS_TX, 9600);
+//    RTCd_Init();
+
+
+    if (SSD1306_Init() == false) {
+        Log_Error("I2C", "Failed to initialize display driver");
+    } else {
+        SSD1306_SetOrientation(true);
+        SSD1306_DispEnable(true);
+    }
     Gui_Init();
+
+    Gps_Init();
+    SpiFlash_WriteUnlock();
+   // Storage_Init();
+    //Stats_Init();
+
     Ramdisk_Init(64000000, "GLogger");
+    addReadme();
     Usbd_MscInit(Ramdisk_GetSectors(), Ramdisk_Read, Ramdisk_Write);
 
-    addReadme();
-
+    Log_Info("System initialized, running main loop");
     while (1) {
         loop();
-        Usbd_MscPool();
+        Usbd_MscPoll();
     }
 }
 
